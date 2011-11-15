0c3f920052cac8581dce36745213ed055b4d8b30
Sajaki <sajaki9@gmail.com>
2011-11-15 18:21:27 +0100

    fixed installer

--- root/language/de/mods/dkp_tooltips.php
+++ root/language/de/mods/dkp_tooltips.php
@@ -49,7 +49,7 @@ $lang = array_merge($lang, array(
     'UMIL_BBCODE_REMOVED' => 'BBCodes entfernt',
 
     'ACP_BBTOOLTIPS' => 'bbTips Einstellungen',
-    'BBTOOLTIPS' => 'bbTips 0.4.0',
+    'BBTOOLTIPS' => 'bbTips 0.4.2',
     'BBTOOLTIPS_EXPLAIN' => 'Tooltips von Wowhead',
 
     'BBTIPS_SETTING' => 'Tooltip Einstellungen',
